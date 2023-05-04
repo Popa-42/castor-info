c9c234738482babfa09cd8560c50ecee468ce74e
Popa <spammailsfuermich@gmail.com>
2023-05-04 19:41:26 +0200

    detusch ist schlöhön
    Alles auf deutsch, extra für Fr. V.

--- castor/game.py
+++ castor/game.py
@@ -6,30 +6,28 @@ from random import shuffle
 
 class Card:
     def __init__(self,
-                 suit: int | None,
-                 value: int):
+                 farbe: int | None,
+                 wert: int):
         """
         Eine Spielkarte mit Farbe und Wert.
 
-        :param suit: Die Farbe der Karte
-        :param value: Der Wert der Karte
+        :param farbe: Die Farbe der Karte
+        :param wert: Der Wert der Karte
         """
-        self.suit = suit
-        self.value = value
+        self.farbe = farbe
+        self.wert = wert
 
     def __repr__(self) -> str:
         """
-        Was bei ``str(Karte)`` bzw ``print(Karte)`` zurückgegeben wird, z.B.:
-
-        >>> karte = Card(suit=1, value=1)
-        >>> print(karte)
-        'Karte(suit=1, value=1)'
+        Was bei ``str(Karte)`` bzw ``print(Karte)`` zurückgegeben wird.
+        Nützlich für den Export des aktuellen Spielstands für die
+        Kommunikation zwischen Client und Server.
 
         :param self: Bezieht sich auf die Instanz der Klasse
         :return: Eine String-Repräsentation des Objekts
         """
-        state = {"suit": self.suit, "value": self.value}
-        return f"{state}"
+        karte = {"farbe": self.farbe, "wert": self.wert}
+        return f"{karte}"
 
     def return_suit(self) -> str:
         """
@@ -38,15 +36,15 @@ class Card:
         :param self: Bezieht sich auf die Instanz der Klasse
         :return: Die Farbe der Karte
         """
-        if self.suit == 0:
+        if self.farbe == 0:
             return "Kreuz"
-        if self.suit == 1:
+        if self.farbe == 1:
             return "Pik"
-        if self.suit == 2:
+        if self.farbe == 2:
             return "Herz"
-        if self.suit == 3:
+        if self.farbe == 3:
             return "Karo"
-        if not self.suit:
+        if not self.farbe:
             return ""
         raise IndexError("Diese Farbe existiert nicht.")
 
@@ -57,38 +55,38 @@ class Card:
         :param self: Bezieht sich auf die Instanz der Klasse
         :return: Der Wert der Karte
         """
-        if self.value == 0:
+        if self.wert == 0:
             return "10"
-        if self.value == 1:
+        if self.wert == 1:
             return "Ass"
-        if self.value == 10:
+        if self.wert == 10:
             return "Bube"
-        if self.value == 11:
+        if self.wert == 11:
             return "Dame"
-        if self.value == 12:
+        if self.wert == 12:
             return "König"
-        if self.value == 50:
+        if self.wert == 50:
             return "Joker"
-        return str(self.value)
+        return str(self.wert)
 
 
 class Player:
     def __init__(self,
-                 number: int,
+                 nummer: int,
                  name: str = "",
                  hand: list[Card, ...] = None):
         """
         Ein Spieler.
 
-        :param number: Die Nummer des Spielers
+        :param nummer: Die Nummer des Spielers
         :param name: Der Name des Spielers
         :param hand: Die Hand des Spielers
         """
         # Nummer (ID) des Spielers
-        self.number = number
+        self.nummer = nummer
         # Name des Spielers
         if not name:
-            name = f"Spieler {self.number}"
+            name = f"Spieler {self.nummer}"
         self.name = name
         # Hand des Spielers
         if not hand:
@@ -96,7 +94,7 @@ class Player:
         self.hand = hand
 
     def __repr__(self):
-        state = {"number": self.number, "name": self.name, "hand": self.hand}
+        state = {"nummer": self.nummer, "name": self.name, "hand": self.hand}
         return f"{state}"
 
     def take_card(self, karte: Card):
@@ -112,7 +110,7 @@ class Player:
 
 class Game:
     def __init__(self,
-                 players: int = 4,
+                 spielerzahl: int = 4,
                  anfangskarten: int = 4,
                  erster_spieler: int = 0):
         """
@@ -123,39 +121,39 @@ class Game:
         - Erstellt ein leeres Deck Karten (der Ablagestapel)
         - Teilt jedem der Spieler (standardmäßig vier) Karten aus
 
-        :param players: Die Anzahl an Spielern
+        :param spielerzahl: Die Anzahl an Spielern
         :param anfangskarten: Die Anzahl an Karten, die jeder Spieler zu Beginn bekommt.
         :param erster_spieler: Der Index desjenigen Spielers, der beginnt
         """
         # Erstelle einen Nachziehstapel (Oberste Karte: Index 0)
-        self.deck: list[Card, ...] = [Card(suit=s, value=v) for s in range(4) for v in range(13)]
+        self.deck: list[Card, ...] = [Card(farbe=s, wert=v) for s in range(4) for v in range(13)]
         self.deck += [Card(None, 50), Card(None, 50)]
         shuffle(self.deck)
         # Erstelle einen Ablagestapel
         self.ablage: list[Card, ...] = []
         # Erstelle die Player
-        self.players = [Player(number=i + 1) for i in range(players)]
+        self.spielerliste = [Player(nummer=i + 1) for i in range(spielerzahl)]
         # Der erste Spieler
-        self.current_player = erster_spieler
+        self.aktueller_spieler = erster_spieler
         # Beginne mit x Karten pro Spieler
         self.anfangskarten = anfangskarten
         # Ob das Spiel gewonnen wurde
-        self.game_won = False
+        self.game_over = False
 
     def __repr__(self) -> str:
-        state = {
+        aktueller_stand = {
             "deck": self.deck,
             "ablage": self.ablage,
-            "players": self.players,
+            "spielerliste": self.spielerliste,
             "anfangskarten": self.anfangskarten,
-            "current_player": self.current_player,
-            "game_won": self.game_won,
+            "aktueller_spieler": self.aktueller_spieler,
+            "game_over": self.game_over,
         }
-        return f"{state}"
+        return f"{aktueller_stand}"
 
     def deal_cards_to_all(self):
         """Gibt jedem Spieler zu Beginn des Spiels seine Karten"""
-        for s in self.players:
+        for s in self.spielerliste:
             for _ in range(self.anfangskarten):
                 s.take_card(self.deck.pop(0))
 
@@ -169,7 +167,12 @@ class Game:
 
     def get_current_player(self) -> Player:
         """Gibt den aktuellen Spieler zurück"""
-        return self.players[self.current_player]
+        return self.spielerliste[self.aktueller_spieler]
+
+    def next_player(self):
+        """Der nächste Spieler"""
+        self.aktueller_spieler += 1
+        self.aktueller_spieler %= len(self.spielerliste)
 
     def export_current_state(self) -> bytes:
         state = self.__repr__()
@@ -179,25 +182,25 @@ class Game:
         # Convert imported bytes to dict
         game: dict = eval(save.decode())
         # Import Players from dict
-        players = game["players"]
-        self.players = [Player(
-            number=p["number"],
-            hand=[Card(suit=c["suit"], value=c["value"]) for c in p["hand"]],
+        spielerliste = game["spielerliste"]
+        self.spielerliste = [Player(
+            nummer=p["nummer"],
+            hand=[Card(farbe=c["farbe"], wert=c["wert"]) for c in p["hand"]],
             name=p["name"]
-        ) for p in players]
+        ) for p in spielerliste]
         # Import deck from dict
         deck = game["deck"]
         if deck:
-            self.deck = [Card(suit=c["suit"], value=c["value"]) for c in deck]
+            self.deck = [Card(farbe=c["farbe"], wert=c["wert"]) for c in deck]
         else:
             self.deck = []
         # Import Ablage from dict
         ablage = game["ablage"]
         if ablage:
-            self.ablage = [Card(suit=c["suit"], value=c["value"]) for c in ablage]
+            self.ablage = [Card(farbe=c["farbe"], wert=c["wert"]) for c in ablage]
         else:
             self.ablage = []
         self.anfangskarten = game["anfangskarten"]
-        self.current_player = game["current_player"]
+        self.aktueller_spieler = game["aktueller_spieler"]
         # Import whether the game was won
-        self.game_won = game["game_won"]
+        self.game_over = game["game_over"]
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # Diese Datei wird ausgeführt
 from castor.game import *
 
-# Achtung: Beim Import wird direkt alles ausgeführt!
+# Achtung: Beim Import wird direkt alles in den Dateien, was nicht unter 'if __name__ == "__main__"' steht, ausgeführt!
 import server_stuff.server as server
 import server_stuff.client as client
 
@@ -9,20 +9,23 @@ if __name__ == '__main__':
     # Das hier drunter wird nur dann beachtet, wenn die Datei selber ausgeführt wird.
     # Wenn man diese Datei importiert und dann versucht auszuführen, passiert nichts.
 
-    game = Game()
-    print(game.players)
+    game = Game(erster_spieler=3)
+    print(game.spielerliste)
     print(game.deck)
     game.deal_cards_to_all()
-    print(game.players)
+    print(game.spielerliste)
     print(game.deck)
     print(game)
-    played_card = game.players[0].play_card_at_index(2)
+    played_card = game.spielerliste[0].play_card_at_index(2)
     game.ablage.append(played_card)
 
     state = game.export_current_state()
     game.import_state(state)
-    print(game.players[2].hand[0].return_suit())
+    print(game.spielerliste[2].hand[0].return_suit())
     print(game.deck[3].return_suit())
-    print(game.ablage[0].return_value())
+    print(game.ablage[0].return_value(), type(game.ablage[0].return_value()))
     print(game.anfangskarten, type(game.anfangskarten))
-    print(game.game_won, type(game.game_won))
+    print(game.game_over, type(game.game_over))
+    print(game.aktueller_spieler, type(game.aktueller_spieler))
+    game.next_player()
+    print(game.aktueller_spieler)
